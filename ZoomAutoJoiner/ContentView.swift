7d16911412efe9cbc7859602d66a060557e1235c
trilokjaikumar <trilokeshwarjaikumar@gmail.com>
2025-11-20 21:29:46 -0500

    Initial Commit

---
+++ ZoomAutoJoiner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZoomAutoJoiner
+//
+//  Created by Trilokeshwar Jaikumar on 11/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZoomAutoJoiner/ZoomAutoJoinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomAutoJoinerApp.swift
+//  ZoomAutoJoiner
+//
+//  Created by Trilokeshwar Jaikumar on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ZoomAutoJoinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
